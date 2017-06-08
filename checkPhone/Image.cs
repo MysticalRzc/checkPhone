bcaa3962cbf1c09d6531474110e98a0ca0374525
MysticalRzc <17862822935@163.com>
2017-06-08 13:45:38 +0800

    回复到以前版本

---
+++ checkPhone/Image.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkPhone
+{
+    class Image
+    {
+        public Bitmap image;
+        public Bitmap processImage;
+        public int width;
+        public int height;
+    }
+}
